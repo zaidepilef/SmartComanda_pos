a00d1f4bcbd1c96622185f514ba95c2abcf5af1b
Felipe Diaz <fel.di.rod@gmail.com>
2026-08-21 13:47:57 -0400

    fix: update environment files to use environment variables for API configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
-  turnstileSiteKey: '1x00000000000000000000AA',
+  apiUrl: '${APIURL}',
+  turnstileSiteKey: '${TURNSTILESITEKEY}',
+  publicUrl: '${PUBLICURL}',
 };
